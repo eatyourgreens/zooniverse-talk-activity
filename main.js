4e9951c01c3261121b1b5ca7ba7a65cd48dd1b5b
Jim O'Donnell <jim@zooniverse.org>
2018-05-23 12:36:01 +0100

    Basic Talk activity app Retrieve the 50 most recent comments from Talk and display the subject images.

---
+++ main.js
@@ -0,0 +1,50 @@
+var comments = []
+var subjects = {};
+
+function renderComment(comment) {
+  var subject = subjects[comment.focus_id];
+  var image_locations = subject.locations.filter(function(location) {
+    var type = Object.keys(location)[0];
+    return type === 'image/jpeg' || type === 'image/png';
+  });
+  var image_location = image_locations[0];
+  var image_url = image_location['image/jpeg'] || image_location['image/png'];
+  console.log(image_url);
+  var img = document.createElement('img');
+  img.src = image_url;
+  img.alt = comment.body;
+  img.title = comment.body;
+  document.body.appendChild(img);
+}
+
+function loadSubjects(subject_ids) {
+  var request = new XMLHttpRequest();
+  request.open('GET', "https://www.zooniverse.org/api/subjects?http_cache=true&page_size=50&id=" + encodeURI(subject_ids.join(',')));
+  request.setRequestHeader('Accept', 'application/vnd.api+json; version=1');
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send();
+  request.onload = function (e) {
+    response = JSON.parse( this.responseText ).subjects;
+    response.forEach(function(subject) {
+      subjects[subject.id] = subject;
+    });
+    comments.forEach(renderComment);
+  }
+}
+(function loadTalkComments() {
+  var request = new XMLHttpRequest();
+  request.open('GET', "https://talk.zooniverse.org/comments?http_cache=true&page_size=50&sort=-created_at&focus_type=Subject");
+  request.setRequestHeader('Accept', 'application/vnd.api+json; version=1');
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send();
+  request.onload = function (e) {
+    comments = JSON.parse( this.responseText ).comments;
+    var subject_ids = comments.map(function(comment) {
+      return comment.focus_id;
+    })
+    subject_ids = subject_ids.filter(function(subject_id, i) {
+      return subject_ids.indexOf(subject_id) === i;
+    });
+    loadSubjects(subject_ids);
+  }
+})();
